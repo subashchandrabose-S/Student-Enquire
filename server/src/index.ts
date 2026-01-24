e0ef0e5fc9c1a8877a74777078412fcd10ee94f9
subash chandra bose <subashchandrabose.s@gmail.com>
2026-01-24 18:27:14 +0530

    Chore: Add health check route

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Health check
+app.get('/', (req, res) => {
+  res.send('Student Enquiry Portal API is running...');
+});
+
 // Routes
 app.use('/api/students', studentRoutes);
 
